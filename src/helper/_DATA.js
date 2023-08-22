75eabe6d38692025d6d41f074a3a731901ebf9cc
ToanLa <lqtoan20@gmail.com>
2023-08-22 21:15:26 +0700

    fix issue related id

--- src/helper/_DATA.js
+++ src/helper/_DATA.js
@@ -155,7 +155,7 @@ function formatQuestion({ optionOneText, optionTwoText, author }) {
   return {
     id: generateUID(),
     timestamp: Date.now(),
-    author,
+    author: author.id,
     optionOne: {
       votes: [],
       text: optionOneText,
