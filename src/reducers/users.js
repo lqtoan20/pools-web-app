d5538e6db29f6c9b3b165c5a5f443e92d1bb8398
ToanLa <lqtoan20@gmail.com>
2023-10-28 23:31:38 +0700

    update

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,7 +3,7 @@ import {
   ADD_QUESTION_USER,
   RECEIVE_USERS,
 } from "../actions/users";
-
+// text
 export default function users(state = {}, action) {
   switch (action.type) {
     case RECEIVE_USERS:
