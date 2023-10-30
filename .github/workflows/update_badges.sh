7083b66719d025afa633796698076f145004bcae
ToanLa <lqtoan20@gmail.com>
2023-10-31 00:40:52 +0700

    test

--- .github/workflows/update_badges.sh
+++ .github/workflows/update_badges.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 badges='<div style="display: flex;">'
 badges+=' <a href="https://fyc-pr-'"$1"'.awsacsnp.autotrader.com/cars-for-sale/all-cars" target="_blank"><img alt="Autotrader" src="https://img.shields.io/badge/Autotrader-100000?style=for-the-badge&logo=&logoColor=F08A23&labelColor=FFFFFF&color=e85b00"/></a>'
 badges+=' <a href="https://fyc-pr-'"$1"'.awsacsnp.autotrader.com/cars-for-sale/all-cars?brand=kbb" target="_blank"><img alt="KBB" src="https://img.shields.io/badge/KBB-100000?style=for-the-badge&logo=&logoColor=F08A23&labelColor=FFFFFF&color=1F3E74"/></a>'
@@ -13,7 +14,7 @@ fi
 pr_body=$(curl -s -H "Authorization: token $2" -X GET "https://api.github.com/repos/$3/pulls/$1" | jq -r ".body")
 
 # Check if the PR body contains the badges and update if not
-if ! echo "$escaped_body" | grep -q "$badges"; then
+if ! echo "$pr_body" | grep -q "$badges"; then
   updated_body="$badges$pr_body"
   escaped_updated_body=$(echo "$updated_body" | jq -s -R -r @uri)
   curl -s -H "Authorization: token $2" -X PATCH "https://api.github.com/repos/$3/pulls/$1" -d '{"body": "'"${escaped_updated_body}"'"}'
