6e1ee0657e1a0faf7c38effef90350d798633a87
ToanLa <lqtoan20@gmail.com>
2023-10-31 00:42:38 +0700

    test lan cuoi

--- .github/workflows/update_badges.sh
+++ .github/workflows/update_badges.sh
@@ -13,11 +13,14 @@ fi
 
 pr_body=$(curl -s -H "Authorization: token $2" -X GET "https://api.github.com/repos/$3/pulls/$1" | jq -r ".body")
 
+# Escape the PR body using urlencode
+escaped_body=$(printf "%s" "$pr_body" | jq -s -R -r @uri)
+
 # Check if the PR body contains the badges and update if not
-if ! echo "$pr_body" | grep -q "$badges"; then
+if ! echo "$escaped_body" | grep -q "$badges"; then
   updated_body="$badges$pr_body"
-  escaped_updated_body=$(echo "$updated_body" | jq -s -R -r @uri)
-  curl -s -H "Authorization: token $2" -X PATCH "https://api.github.com/repos/$3/pulls/$1" -d '{"body": "'"${escaped_updated_body}"'"}'
+  escaped_updated_body=$(printf "%s" "$updated_body" | jq -s -R -r @uri)
+  curl -s -H "Authorization: token $2" -X PATCH "https://api.github.com/repos/$3/pulls/$1" -d "{\"body\": \"$escaped_updated_body\"}"
 else
   echo 'Badges already exist for this PR'
 fi
