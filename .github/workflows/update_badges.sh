b1b7bffa02eaf58465c00342daedc3b78a6d7194
ToanLa <lqtoan20@gmail.com>
2023-10-31 00:36:13 +0700

    add

--- .github/workflows/update_badges.sh
+++ .github/workflows/update_badges.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-badges='<div style="display: flex;">'
-badges+=' <a href="https://fyc-pr-'"$1"'.awsacsnp.autotrader.com/cars-for-sale/all-cars" target="_blank"><img alt="Autotrader" src="https://img.shields.io/badge/Autotrader-100000?style=for-the-badge&logo=&logoColor=F08A23&labelColor=FFFFFF&color=e85b00"/></a>'
-# ...
-badges+=' </div>\n\n'
-
 badges='<div style="display: flex;">'
 badges+=' <a href="https://fyc-pr-'"$1"'.awsacsnp.autotrader.com/cars-for-sale/all-cars" target="_blank"><img alt="Autotrader" src="https://img.shields.io/badge/Autotrader-100000?style=for-the-badge&logo=&logoColor=F08A23&labelColor=FFFFFF&color=e85b00"/></a>'
 badges+=' <a href="https://fyc-pr-'"$1"'.awsacsnp.autotrader.com/cars-for-sale/all-cars?brand=kbb" target="_blank"><img alt="KBB" src="https://img.shields.io/badge/KBB-100000?style=for-the-badge&logo=&logoColor=F08A23&labelColor=FFFFFF&color=1F3E74"/></a>'
